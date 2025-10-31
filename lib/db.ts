39c00228b5087ab2add973f9f9ee87dd66f7ffbf
ivanttcas <ivancastelo25@gmail.com>
2025-10-30 21:00:31 -0500

    EM-01: add mock data and type definitions

---
+++ lib/db.ts
@@ -0,0 +1,46 @@
+import { Product } from '../types/products' 
+
+export const ProductList: Product[] = [
+    {
+        id: 1,
+        img: '',
+        name:'3kg Croquetas',
+        description: 'Croquetas de cordero balanceado para perro',
+        price: 35,
+    },
+    {
+        id: 2,
+        img: '',
+        name:'7kg Croquetas',
+        description: 'Croquetas de pollo y carne balanceado para perro',
+        price: 75,
+    },
+    {
+        id: 3,
+        img: '',
+        name:'Shampoo Canino',
+        description: 'Formula antipulgas y suave para la piel',
+        price: 20,
+    },
+    {
+        id: 4,
+        img: '',
+        name:'Bravecto',
+        description: 'Pastilla masticable antipulgas y garrapatas',
+        price: 140,
+    },
+    {
+        id: 5,
+        img: '',
+        name:'Rascador para gatos',
+        description: 'Ideal para combatir el aburrimiento de tu peludo',
+        price: 32,
+    },
+    {
+        id: 6,
+        img: '',
+        name:'Premios Biscuit',
+        description: 'Galletas salada para premiar a tu canino',
+        price: 20,
+    },
+]
---
+++ types/products.ts
@@ -0,0 +1,7 @@
+export type Product = {
+    id: number;
+    img: string;
+    name: string;
+    description: string;
+    price: number;
+};
